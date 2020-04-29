f7c01c7c590de8520b131ab8df0aeb8283da0423
singhravindra <ravindra.singh29@gmail.com>
2020-04-29 19:58:07 +0530

    Removing Hardcoding's

---
+++ src/main/java/com/taxSlab/TaxCalculator/IncomeSlabConstants.java
@@ -0,0 +1,11 @@
+package com.taxSlab.TaxCalculator;
+
+public class IncomeSlabConstants {
+
+	public static int ZERO = 0;
+	public static int TEN_THOUSAND = 10000;
+	public static int TWENTY_THOUSAND = 20000;
+	public static double PERCENTAGE_10 = 0.1;
+	public static double PERCENTAGE_20 = 0.2;
+	
+}
--- src/main/java/com/taxSlab/TaxCalculator/TaxCalculatorService.java
+++ src/main/java/com/taxSlab/TaxCalculator/TaxCalculatorService.java
@@ -7,14 +7,14 @@ public class TaxCalculatorService {
 
 	private double taxableAmount=0; 
 	public double calculateTaxSlab(int enterIncome) throws Exception{
-		if(enterIncome<=0){
+		if(enterIncome<=IncomeSlabConstants.ZERO){
 			throw new IncomeNotFoundException("Invalid Amount : " + enterIncome);
-		}else if(enterIncome>0 && enterIncome<=10000){
+		}else if(enterIncome>IncomeSlabConstants.ZERO && enterIncome<=IncomeSlabConstants.TEN_THOUSAND){
 			taxableAmount = enterIncome;
-		}else if(enterIncome>10000 && enterIncome<=20000){
-			taxableAmount = enterIncome * 0.1;
-		}else if(enterIncome>20000){
-			taxableAmount = enterIncome * 0.2;
+		}else if(enterIncome>IncomeSlabConstants.TEN_THOUSAND && enterIncome<=IncomeSlabConstants.TWENTY_THOUSAND){
+			taxableAmount = enterIncome * IncomeSlabConstants.PERCENTAGE_10;
+		}else if(enterIncome>IncomeSlabConstants.TWENTY_THOUSAND){
+			taxableAmount = enterIncome * IncomeSlabConstants.PERCENTAGE_20;
 		}
 		return taxableAmount;
 	
